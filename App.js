a44a5f81ab557f193bfa97564fc09efff66991f3
Simon Zhang <zzxzzxsimon@gmail.com>
2021-01-13 20:38:37 -0800

    set up bottom navigation bar and established the framworks for the app

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
 import { StatusBar } from "expo-status-bar";
 import React, { Component } from "react";
-
 import { StyleSheet, Text, View } from "react-native";
 
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import * as firebase from "firebase";
+
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import rootReducer from "./redux/reducers";
+import thunk from "redux-thunk";
+
+import AddScreen from "./components/main/Add";
+
+const store = createStore(rootReducer, applyMiddleware(thunk));
+
 const firebaseConfig = {
   apiKey: "AIzaSyAlCIRSgj8lx3K-uKeZHhJ4hu7EtaD2xR8",
   authDomain: "instagram-demo-3418b.firebaseapp.com",
@@ -22,6 +31,8 @@ if (firebase.app.length === 0) {
 import LandingScreen from "./components/auth/Landing";
 import RegisterScreen from "./components/auth/Register";
 import LoginScreen from "./components/auth/Login";
+import MainScreen from "./components/Main";
+import { NativeScreen } from "react-native-screens";
 
 const Stack = createStackNavigator();
 
@@ -62,7 +73,7 @@ export class App extends Component {
           <Stack.Navigator initialRouteName="Landing">
             <Stack.Screen
               name="Landing"
-              component={LandingScreen}
+              component={LandinsgScreen}
               options={{ headerShown: false }}
             />
             <Stack.Screen name="Register" component={RegisterScreen} />
@@ -71,9 +82,18 @@ export class App extends Component {
       );
     }
     return (
-      <View style={{ flex: 1, justifyContent: "center" }}>
-        <Text>Userr is logged in</Text>
-      </View>
+      <Provider store={store}>
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName="Main">
+            <Stack.Screen
+              name="Main"
+              component={MainScreen}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen name="Add" component={AddScreen} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </Provider>
     );
   }
 }
